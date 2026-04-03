96a7ced6deb230512f60a1e2a2a056719421aea6
Tiago Braga <ztiagok@gmail.com>
2026-04-03 15:19:56 -0300

    refactor: Refatorando código para utilização de funções e completando requisito 3.

--- file.py
+++ file.py
@@ -3,15 +3,36 @@
 # Professor: Marcelo Collares 
 # Data: 03/Abril/2026
 
-answer: int = 0
-student: list[dict] = {}
+def main():
+  answer: int = 0
+  students: list[dict] = []
 
-# Enquanto a resposta for menor que 1 ou maior que 5, será mostrado o print e o input.
-while (int(answer) != 5):
-  print("\n1 - Cadastrar aluno\n" \
-        "2 - Listar alunos\n" \
-        "3 - Calcular média da turma \n" \
-        "4 - Buscar aluno por nome \n" \
-        "5 - Sair")
+  # Enquanto a resposta for menor que 1 ou maior que 5, será mostrado o print e o input.
+  while (int(answer) != 5):
+    print("\n1 - Cadastrar aluno\n" \
+          "2 - Listar alunos\n" \
+          "3 - Calcular média da turma \n" \
+          "4 - Buscar aluno por nome \n" \
+          "5 - Sair")
 
-  answer = input("Selecione uma opção: ")
+    answer = int(input("Selecione uma opção: "))
+
+    match(answer):
+      case 1:
+        cadastrarAluno(students)
+
+
+def cadastrarAluno(students: list[dict]):
+  student_name = input("\nNome do aluno: ")
+  student_grade_1 = input("Nota 1 do aluno: ")
+  student_grade_2 = input("Nota 2 do aluno: ")
+
+  students.append({
+    "nome": student_name,
+    "nota1": float(student_grade_1),
+    "nota2": float(student_grade_2),
+    })
+
+
+if __name__ == "__main__":
+  main()
