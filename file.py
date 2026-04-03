45740cf6b393fa55d5539d8e37aa79749db1f76b
Tiago Braga <ztiagok@gmail.com>
2026-04-03 14:47:44 -0300

    feat: Requisito 2 concluído.

--- file.py
+++ file.py
@@ -2,3 +2,12 @@
 # Disciplina: Introdução à Programação 
 # Professor: Marcelo Collares 
 # Data: 03/Abril/2026
+
+answer: int = 0
+
+# Enquanto a resposta for menor que 1 ou maior que 5, será mostrado o print e o input.
+while (int(answer) < 1 or int(answer) > 5):
+  print("\n1 - Cadastrar aluno\n2 - Listar alunos\n3 - Calcular m0" \
+  "édia da turma\n4 - Buscar aluno por nome\n5 - Sair")
+
+  answer = input("Selecione uma opção: ")
