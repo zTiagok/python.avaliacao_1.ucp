89c438a122b111ab1887dcb6ea5261301d36553d
Tiago Braga <ztiagok@gmail.com>
2026-04-03 15:06:54 -0300

    refactor: Reescrevendo tabela de opções para melhor visibilidade.

--- file.py
+++ file.py
@@ -4,10 +4,14 @@
 # Data: 03/Abril/2026
 
 answer: int = 0
+student: list[dict] = {}
 
 # Enquanto a resposta for menor que 1 ou maior que 5, será mostrado o print e o input.
 while (int(answer) != 5):
-  print("\n1 - Cadastrar aluno\n2 - Listar alunos\n3 - Calcular m" \
-  "édia da turma\n4 - Buscar aluno por nome\n5 - Sair")
+  print("\n1 - Cadastrar aluno\n" \
+        "2 - Listar alunos\n" \
+        "3 - Calcular média da turma \n" \
+        "4 - Buscar aluno por nome \n" \
+        "5 - Sair")
 
   answer = input("Selecione uma opção: ")
