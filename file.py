ae2cbececd9c24d5e08137c351fb36e694bd51ba
Tiago Braga <ztiagok@gmail.com>
2026-04-03 14:53:33 -0300

    refactor: Mudando lógica do requisito 1 para programar só parar de loopar quando a resposta for "5".

--- file.py
+++ file.py
@@ -6,12 +6,8 @@
 answer: int = 0
 
 # Enquanto a resposta for menor que 1 ou maior que 5, será mostrado o print e o input.
-while (int(answer) < 1 or int(answer) > 5):
+while (int(answer) != 5):
   print("\n1 - Cadastrar aluno\n2 - Listar alunos\n3 - Calcular m" \
   "édia da turma\n4 - Buscar aluno por nome\n5 - Sair")
 
   answer = input("Selecione uma opção: ")
-
-  match(int(answer)):
-    case 5:
-      break
